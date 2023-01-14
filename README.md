3388f94e21c894f39f4f9b9593e4c0161a97cbb2
mauricevalerio <75749759+mauricevalerio@users.noreply.github.com>
2023-01-14 02:17:54 -0800

    initial files

---
+++ README.md
@@ -0,0 +1,9 @@
+# Responsive Design Learning Journal
+ Scrimba Learning Journal HTML, CSS, Responsive Design, & JS
+
+ [Learning Journal]()
+ 
+ ## Added the following features
+    - Added bootstrap for the hamburger menu icons
+	- Added "View more/less" functionality
+
---
+++ data.js
@@ -0,0 +1,42 @@
+export const blogsArrayData = [
+    {
+        imgPath: 'images/article-image-01.png',
+        date: 'January 2, 2023',
+        title: 'Very 1st blog',
+        content: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path."
+    },
+    {
+        imgPath: 'images/article-image-02.png',
+        date: 'January 9, 2023',
+        title: 'Keep going...',
+        content: "Be consistent, be disciplined, and set a schedule to study and avoid all distractions."
+    },
+    {
+        imgPath: 'images/article-image-03.png',
+        date: 'January 16, 2023',
+        title: 'The exciting and boring parts',
+        content: `There are times when I get bored and tired in a specific module or topic that is when I decide to take a break.
+				I'll go back when I feel I am ready to learn something new again and make small projects to make it exciting.`
+    },
+    {
+        imgPath: 'images/article-image-04.png',
+        date: 'January 23, 2023',
+        title: 'Be contented and be open to other ideas',
+        content: `I know it can be tempting to constantly improve projects preventing you from publishing it online.
+				You can always ask for improvements, suggestions, and code reviews if you're out of ideas.`
+    },
+    {
+        imgPath: 'images/article-image-05.png',
+        date: 'January 30, 2023',
+        title: 'Practice xINFINITY',
+        content: `Building small projects on your own is always the best teacher when it comes to coding. It lets you think critically and logically independently
+		taking into account the concepts you've learned`
+    },
+    {
+        imgPath: 'images/article-image-06.png',
+        date: 'January 31, 2023',
+        title: 'Blog six',
+        content: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur tempus imperdiet tincidunt. 
+		Donec vel libero finibus, scelerisque ligula ac, viverra nunc. Donec a imperdiet enim, et rhoncus leo. `
+    },
+]
